6e8e0ff6bf1f55ca25e88e7824588f3a08ca7563
Rajeshwar Polaty <rrao.polaty@gmail.com>
2018-06-29 12:33:45 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+const hbs = require('hbs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear', ()=>{
+
+  return new Date();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+  return text.toUpperCase();
+});
+
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next)=>{
+    res.render('maintenance.hbs', {
+      pageTitle:"Maintenance page"
+    })
+});
+
+
+app.get('/',(req, res)=>{
+  res.send('Hello!! This is Root folder');
+});
+
+app.get('/header',(req, res)=>{
+  res.send({"name": "Rajeshwar",
+            "likes": ["Tennis", "Hiking","Biking"]});
+});
+
+app.get('/home',(req, res)=>{
+  res.render('home.hbs',{
+            "pageTitle": "About page from server",
+            "name": "Rajeshwar",
+            "myHobbies": ["Tennis", "Hiking","Biking"],
+            "copyrightYear": new Date().getFullYear()});
+});
+
+app.get('/about',(req, res)=>{
+  res.render('about.hbs',{
+    pageTitle: "About page from server",
+    copyrightYear: new Date().getFullYear()
+  });
+});
+
+app.listen(3000);
